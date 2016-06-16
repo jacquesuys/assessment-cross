373aebd8beaf03299668493a00305ae692fe2208
Jacques Uys <jaxuys@gmail.com>
2016-06-16 17:41:30 +0200

    mvp toggle for accordion

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 
 angular.module('crossApp', [])
   .controller('MainCtrl', function ($scope, $http) {
+    // needs to be a service
     $http({
       method: 'GET',
       url: 'data/data.json'
     }).then(function(resp) {
       $scope.tests = resp.data;
     });
+
+    $scope.isOpen = '';
+
+    $scope.toggle = function($index) {
+      $scope.isOpen = $index;
+    };
+
   });
